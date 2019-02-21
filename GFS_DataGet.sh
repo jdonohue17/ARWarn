3df11b46228b6283b287dc4bb21e71e3eb5dfc18
connorbrown <connorbrown@u.boisestate.edu>
2019-02-21 14:25:38 -0700

    Check for empty arguments and unmade GFSData directory

--- GFS_DataGet.sh
+++ GFS_DataGet.sh
@@ -11,6 +11,12 @@ BOTLAT="-90"
 FORECAST_CYCLE=$1
 FILENAME="./GFSData/gfs_${NOW}_${FORECAST_CYCLE}.grb"
 
+if [ -z "$1" ]
+	then 
+		echo "Usage ./GFSDataGet.sh forecast_cycle"
+		exit
+fi
 
+mkdir -p ./GFSData
 
 wget "https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl?file=gfs.t${FORECAST_CYCLE}z.pgrb2.0p25.anl&all_lev=on&var_SPFH=on&var_TMAX=on&var_TMIN=on&var_TMP=on&var_UGRD=on&var_VGRD=on&subregion=&leftlon=${LEFTLONG}&rightlon=${RIGHTLONG}&toplat=${TOPLAT}&bottomlat=${BOTLAT}&dir=%2Fgfs.${NOW}${FORECAST_CYCLE}" --output-document=$FILENAME
