99b7d557bc0da5d1b071cdca912d637d34601c17
connorbrown <connorbrown@u.boisestate.edu>
2019-02-21 18:04:37 -0700

    fix GRB_Cleanup.sh

--- GRB_Cleanup.sh
+++ GRB_Cleanup.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-$DATE=$(date +%Y%m%d)
-$PATH="./GFSData"
-$FILENAME="*.grb"
-$MTIME="+1"
-$LOGNAME="$PATH/log_${DATE}.txt"
+#Author: Connor BRown
+# 21 Feb 2019
 
-find $PATH -name $FILENAME -type f -mtime $MTIME >> $LOGNAME
+DATE=$(date +"%Y%m%d")
+DATA_DIR="./GFSData"
+FILENAME="*.grb"
+#MTIME="+1"
+MMIN="+10"
+LOGNAME="$DATA_DIR/log_${DATE}.txt"
 
+mkdir -p $DATA_DIR
+
+#find $DATA_DIR -name $FILENAME -type f -mtime $MTIME >> $LOGNAME
+
+find $DATA_DIR -name $FILENAME -type f -mmin $MMIN >> $LOGNAME
 
