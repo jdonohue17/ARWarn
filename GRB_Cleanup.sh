e9a9b8e2413899843353344c95263fa5fe70ce77
connorbrown <connorbrown@u.boisestate.edu>
2019-03-26 14:09:33 -0600

    change cleanup script to delete files

--- GRB_Cleanup.sh
+++ GRB_Cleanup.sh
@@ -6,13 +6,13 @@
 DATE=$(date +"%Y%m%d")
 DATA_DIR="./SeniorProject/ARWarn/GFSData"
 FILENAME="*.grb"
-#MTIME="+1"
-MMIN="+10"
+MTIME="+1"
+#MMIN="+10"
 LOGNAME="$DATA_DIR/log_${DATE}.txt"
 
 mkdir -p $DATA_DIR
 
-#find $DATA_DIR -name $FILENAME -type f -mtime $MTIME >> $LOGNAME
+find $DATA_DIR -name $FILENAME -type f -mtime $MTIME -delete -maxdepth 1 >> $LOGNAME
 
-find $DATA_DIR -name $FILENAME -type f -mmin $MMIN >> $LOGNAME
+#find $DATA_DIR -name $FILENAME -type f -mmin $MMIN >> $LOGNAME
 
