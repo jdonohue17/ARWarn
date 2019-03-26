507c8205dd22f3ee4dc6d76abfd205aa8cdf0728
root <root@panther-lxp-01.boisestate.edu>
2019-03-26 15:43:48 -0600

    Add grb to netcdf conversion

--- GFS_DataGet.sh
+++ GFS_DataGet.sh
@@ -22,3 +22,5 @@ mkdir -p ./GFSData
 curl -f "https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl?file=gfs.t${FORECAST_CYCLE}z.pgrb2.0p25.anl&all_lev=on&var_SPFH=on&var_TMAX=on&var_TMIN=on&var_TMP=on&var_UGRD=on&var_VGRD=on&subregion=&leftlon=${LEFTLONG}&rightlon=${RIGHTLONG}&toplat=${TOPLAT}&bottomlat=${BOTLAT}&dir=%2Fgfs.${NOW}${FORECAST_CYCLE}" -o $FILENAME
 
 echo "Saving to $FILENAME"
+
+ncl_convert2nc $FILENAME
