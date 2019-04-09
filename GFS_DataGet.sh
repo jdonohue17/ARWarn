726023b791205b967e5846d1b7d5389c009e973b
root <root@panther-lxp-01.boisestate.edu>
2019-04-09 13:38:59 -0600

    Add cleanup of images and netcdf files and update dataget

--- GFS_DataGet.sh
+++ GFS_DataGet.sh
@@ -10,6 +10,9 @@ TOPLAT="90"
 BOTLAT="-90"
 FORECAST_CYCLE=$1
 FILENAME="./GFSData/gfs_${NOW}_${FORECAST_CYCLE}.grb"
+FILENCNAME="./GFSData/gfs_${NOW}_${FORECAST_CYCLE}.nc"
+PLOTSCRIPT="/opt/python-user/cs_student_project/plotar.py"
+PLOTOUTPUTFILE="/opt/python-user/html/images/${NOW}${FORECAST_CYCLE}.png"
 
 if [ -z "$1" ]
 	then 
@@ -18,9 +21,12 @@ if [ -z "$1" ]
 fi
 
 mkdir -p ./GFSData
+mkdir -p opt/python-user/html/images
 
 curl -f "https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl?file=gfs.t${FORECAST_CYCLE}z.pgrb2.0p25.anl&all_lev=on&var_SPFH=on&var_TMAX=on&var_TMIN=on&var_TMP=on&var_UGRD=on&var_VGRD=on&subregion=&leftlon=${LEFTLONG}&rightlon=${RIGHTLONG}&toplat=${TOPLAT}&bottomlat=${BOTLAT}&dir=%2Fgfs.${NOW}${FORECAST_CYCLE}" -o $FILENAME
 
 echo "Saving to $FILENAME"
 
-ncl_convert2nc $FILENAME
+ncl_convert2nc $FILENAME -o ./GFSData
+
+$PLOTSCRIPT $FILENCNAME $PLOTOUTPUTFILE
