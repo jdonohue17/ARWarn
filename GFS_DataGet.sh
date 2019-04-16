c96caad6032e47df091c0c5eed0758a5f396dd92
root <root@panther-lxp-01.boisestate.edu>
2019-04-16 13:53:35 -0600

    Add comments to scripts

--- GFS_DataGet.sh
+++ GFS_DataGet.sh
@@ -2,18 +2,23 @@
 
 #Author : Connor Brown
 # 19 Feb 2019
+# Downloads grb files from noaa, converts them to netcdf files, then plots them
 
 NOW=$(date +"%Y%m%d")
+FORECAST_CYCLE=$1
+
 LEFTLONG="0"
 RIGHTLONG="360"
 TOPLAT="90"
 BOTLAT="-90"
-FORECAST_CYCLE=$1
+
 FILENAME="./GFSData/gfs_${NOW}_${FORECAST_CYCLE}.grb"
 FILENCNAME="./GFSData/gfs_${NOW}_${FORECAST_CYCLE}.nc"
+
 PLOTSCRIPT="/opt/python-user/cs_student_project/plotar.py"
 PLOTOUTPUTFILE="/opt/python-user/html/images/${NOW}${FORECAST_CYCLE}.png"
 
+#input validation
 if [ -z "$1" ]
 	then 
 		echo "Usage ./GFSDataGet.sh forecast_cycle"
@@ -23,10 +28,13 @@ fi
 mkdir -p ./GFSData
 mkdir -p opt/python-user/html/images
 
+#download grb
 curl -f "https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl?file=gfs.t${FORECAST_CYCLE}z.pgrb2.0p25.anl&all_lev=on&var_SPFH=on&var_TMAX=on&var_TMIN=on&var_TMP=on&var_UGRD=on&var_VGRD=on&subregion=&leftlon=${LEFTLONG}&rightlon=${RIGHTLONG}&toplat=${TOPLAT}&bottomlat=${BOTLAT}&dir=%2Fgfs.${NOW}${FORECAST_CYCLE}" -o $FILENAME
 
 echo "Saving to $FILENAME"
 
+#convert to netcdf
 ncl_convert2nc $FILENAME -o ./GFSData
 
+#plot and output image to images folder
 $PLOTSCRIPT $FILENCNAME $PLOTOUTPUTFILE
--- GRB_Cleanup.sh
+++ GRB_Cleanup.sh
@@ -2,22 +2,24 @@
 
 #Author: Connor BRown
 # 21 Feb 2019
+# Searches the data directory for grb files older than MTIME and deletes them
 
-DATE=$(date +"%Y%m%d")
 DATA_DIR="./GFSData"
 IMG_DIR="/opt/python-user/html/images"
 FILENAME="*.grb"
 NETCDFNAME="*.nc"
 IMGNAME="*.png"
 MTIME="+30"
+
 #MMIN="+10"
+DATE=$(date +"%Y%m%d")
 LOGNAME="$DATA_DIR/log_${DATE}.txt"
 
 mkdir -p $DATA_DIR
 
 find $DATA_DIR -name $FILENAME -type f -mtime $MTIME -delete
 
-find $DATA_DIR -name $NETCDFNAME -type f -mtime $MTIME -delete
+find $DATA_DIR -name $NETCDFNAME -type f -mtime +1 -delete
 
 find $IMG_DIR -name $IMGNAME -type f -mtime $MTIME -delete
 
